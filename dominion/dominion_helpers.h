c81164cad428ab512881c1049384981d693a7c8d
Will <willdarnell@gmail.com>
2019-10-13 15:47:12 -0500

    refactor baron

--- dominion/dominion_helpers.h
+++ dominion/dominion_helpers.h
@@ -12,6 +12,7 @@ int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3,
                struct gameState *state, int handPos, int *bonus);
 int mine(int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus, int j, int currentPlayer);
+int baron(int currentPlayer, struct gameState *state);
 
 
 #endif
