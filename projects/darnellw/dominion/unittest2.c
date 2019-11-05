e73cb064e387ccc5fe22d9b4834e60785cd845ee
willdarnell <darnellw@flip1.engr.oregonstate.edu>
2019-11-04 18:50:44 -0800

    some tests done for baron

--- projects/darnellw/dominion/unittest2.c
+++ projects/darnellw/dominion/unittest2.c
@@ -10,19 +10,37 @@
 //int baron_function(int currentPlayer, struct gameState *state, int choice1)
 int main () {
 
-int choice1 = 0;
+int choice1 = 1;
 int currentPlayer = 0;
 //setting up the test case taken from the example
 struct gameState G, testG;
 int numPlayers = 2;
 int seed = 1000;
-int k[10] = {copper, gold, silver, minion, mine, cutpurse,
+int k[10] = {estate, gold, silver, minion, mine, cutpurse,
 			sea_hag, tribute, smithy, council_room};
 initializeGame(numPlayers, k, seed, &G);
 memcpy(&testG, &G, sizeof(struct gameState));
 
 printf("%s", "This is the test for the baron card\n");
 baron_function(currentPlayer, &testG, choice1);
+
+//test for number of buys increasing by one
+
+if (testG.numBuys == ++G.numBuys){
+    printf("%s", "the number of buys was incremented correctly.\n");
+}
+else {
+     printf("%s", "the number of buys was NOT incremented correctly.\n");
+}
+
+//this function finds the first bug that I entered, which was changing the boolean flag to start as false, which 
+//made the first branch never execute.
+if ((testG.coins == G.coins) && (testG.supplyCount[estate] == G.supplyCount[estate])){
+     printf("%s", "the function is not working properly and did not execute.");
+}
+else {
+     printf("%s", "at least one branch of the function ran correctly.");
+}
 
 return 0;
 }
