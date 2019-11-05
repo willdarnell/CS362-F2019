76363f8e35c3a86cb736a6c78be647b3cfc82e92
willdarnell <darnellw@flip1.engr.oregonstate.edu>
2019-11-04 19:33:45 -0800

    finish unit test 2

--- projects/darnellw/dominion/dominion.c
+++ projects/darnellw/dominion/dominion.c
@@ -728,7 +728,7 @@ int baron_function(int currentPlayer, struct gameState *state, int choice1)
     state->numBuys++;//Increase buys by 1!
     if (choice1 > 0) { //Boolean true or going to discard an estate
         int p = 0;//Iterator for hand!
-        int card_not_discarded = 0;//Flag for discard set!
+        int card_not_discarded = 1;//Flag for discard set!
         while(card_not_discarded) {
             if (state->hand[currentPlayer][p] == estate) { //Found an estate card!
                 state->coins -= 4;//Add 4 coins to the amount of coins
--- projects/darnellw/dominion/unittest2.c
+++ projects/darnellw/dominion/unittest2.c
@@ -34,13 +34,54 @@ else {
 }
 
 //this function finds the first bug that I entered, which was changing the boolean flag to start as false, which 
-//made the first branch never execute.
+//made the first branch never execute but I am going to change that flag back in the source code so that I can 
+//achieve test coverage.
 if ((testG.coins == G.coins) && (testG.supplyCount[estate] == G.supplyCount[estate])){
-     printf("%s", "the function is not working properly and did not execute.");
+     printf("%s", "the function is not working properly and did not execute.\n");
 }
 else {
-     printf("%s", "at least one branch of the function ran correctly.");
+     printf("%s", "at least one branch of the function ran correctly.\n");
 }
 
+//check the number of coins math
+//this test finds the second bug that I added, changing the coin change from + 4 to -4. 
+if (testG.coins == (G.coins + 4)){
+    printf("%s", "The function is doing the math correctly since there is one estate card in the players hand.\n");
+}
+else {
+    printf("%s", "the function is not doing the math correctly in terms of coins.\n");
+
+}
+choice1 = 0;
+//the case for executing the second branch
+baron_function(currentPlayer, &testG, choice1);
+
+
+//another test case for if the second player gains the estate card
+choice1 = 1;
+int j[10] = {curse, gold, silver, minion, mine, cutpurse,
+			sea_hag, tribute, estate, council_room};
+initializeGame(numPlayers, j, seed, &G);
+struct gameState testF;
+memcpy(&testF, &G, sizeof(struct gameState));
+testF.supplyCount[estate] = 1;
+
+baron_function(1, &testF, choice1);
+G.supplyCount[estate] = 1;
+
+//test to see if the supply count is double decremented when supply is 1. is a bug in the gain card function.
+if (testF.supplyCount[estate] == (G.supplyCount[estate] - 2)){
+    printf("%s", "The estate card was successfully removed from the supply.\n");
+    printf("%d", testF.supplyCount[estate]);
+    printf("%d", G.supplyCount[estate]);
+
+
+
+}
+else {
+    printf("%s", "the estate card was not successfully removed from the supply.\n");
+    printf("%d", testF.supplyCount[estate]);
+    printf("%d", G.supplyCount[estate]);
+}
 return 0;
 }
