db56291b551b5e8cbf6539ca095e3124b3a9e1cd
willdarnell <darnellw@flip1.engr.oregonstate.edu>
2019-11-04 20:22:04 -0800

    updates to unit test 3

--- projects/darnellw/dominion/unittest3.c
+++ projects/darnellw/dominion/unittest3.c
@@ -11,6 +11,7 @@ int main (){
 int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0, j = 0;
 struct gameState G, testG;
 int numPlayers = 2;
+int thisPlayer = 1;
 int seed = 1000;
 int k[10] = {copper, gold, silver, minion, mine, cutpurse,
 			sea_hag, tribute, smithy, council_room};
@@ -24,11 +25,17 @@ printf("%s", "This is the test for the minion card.\n");
 minion_function(choice1, choice2, 0, &testG, handpos, 0);
 if (testG.numActions == ++G.numActions){
     printf("%s", "The number of actions was incremented correctly.\n");
+    printf("%d", testG.handCount[1]);
 }
 else {
     printf("%s", "The number of actions was not incremented correctly.\n");
 
 }
+
+//don't take the coins, instead discard four cards
+testG.handCount[1] = 8;
+printf("%d", testG.handCount[1]);
+minion_function(choice1, choice2, 0, &testG, handpos, 0);
 
 
 
@@ -39,7 +46,8 @@ else {
 //change the decision to take the coins
 choice1 = 1;
 minion_function(choice1, choice2, 0, &testG, handpos, 0);
-//second test to see if the coins is increased by 2 if the first choice is taken
+//second test to see if the coins is increased by 2 if the first choice is taken, which reveals the first bug
+//that I added for the minion function. This test fails because I changed the conditional to not increment the coins.
 if (testG.coins == (G.coins + 2)){
     printf("%s", "The number of coins was increased correctly.\n");
 }
