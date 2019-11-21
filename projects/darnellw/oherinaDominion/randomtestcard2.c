25a4ed41963fa204f205ef13b39c6eb1025475e0
willdarnell <darnellw@flip3.engr.oregonstate.edu>
2019-11-21 12:38:41 -0800

    add random tests to folder/fix signatures

---
+++ projects/darnellw/oherinaDominion/randomtestcard2.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+
+
+
+int main(){
+
+    int i = 0;
+    for (i; i < 100; i++){
+        int choice1 = rand() % 4;
+        int choice2 = rand() % 4;
+
+        int currentPlayer = rand() % 4;
+        //setting up the test case taken from the example
+        struct gameState G, testG;
+        int numPlayers = rand() % 3;
+        int thisPlayer = rand() % 3;
+        int handpos = rand() % 3;
+
+        int seed = 1000;
+        int z = 0;
+        int k[10];
+        for (z; z < 10; z++){
+            k[z] = rand() % 27;
+        }
+        
+        
+        initializeGame(numPlayers, k, seed, &G);
+        
+        memcpy(&testG, &G, sizeof(struct gameState));
+
+        printf("%s", "This is the test for the minion card.\n");
+
+        //first test to see if the actions are correctly implemented
+        cardEffectMinion(choice1, choice2, &testG, handpos, 0);
+        if (testG.numActions == ++G.numActions){
+        printf("%s", "The number of actions was incremented correctly.\n");
+        }
+        else {
+            printf("%s", "The number of actions was not incremented correctly.\n");
+
+        }
+        //second test to see if the coins is increased by 2 if the first choice is taken, which reveals the first bug
+        //that I added for the minion function. This test fails because I changed the conditional to not increment the coins.
+        if (testG.coins == (G.coins + 2)){
+            printf("%s", "The number of coins was increased correctly.\n");
+        }
+        else {
+            printf("%s", "The number of coins was not increased correctly.\n");
+
+        }
+    }
+    
+
+
+return 0;
+
+
+
+
+}
---
+++ projects/darnellw/oherinaDominion/randomtestcard3.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+
+
+
+int main(){
+
+    int i = 0;
+    for (i; i < 100; i++){
+        int currentPlayer = rand() % 3;
+        //setting up the test case taken from the example
+        struct gameState G, testG;
+        int nextPlayer = rand() % 2;
+        int numPlayers = rand() % 2;
+        int handpos = 0;
+
+        int seed = 1000;
+        int z = 0;
+        int tributeRevealedCards[2];
+        int k[10];
+        for (z; z < 10; z++){
+            k[z] = rand() % 27;
+        }
+        
+        initializeGame(numPlayers, k, seed, &G);
+        memcpy(&testG, &G, sizeof(struct gameState));
+
+        printf("%s", "This is the test for the tribute card\n");
+        cardEffectTribute(nextPlayer, &testG, handpos, currentPlayer);
+        if (tributeRevealedCards[1] == -1){
+            printf("%s", "There is a bug in the tribute logic.\n");
+        }
+        else {
+            printf("%s""%d", "The check to see if the cards are the same that are revealed works.\n"), tributeRevealedCards[0];
+
+        }
+        if (tributeRevealedCards[1] == 13){
+            printf("%s", "The tribute funciton is working correctly.\n");
+        }
+        else {
+            printf("%s""%d", "The tribute function is not correctly assigning tribute revealed cards.\n"), tributeRevealedCards[1];
+
+        }
+        if (testG.deckCount[1] == 0){
+            printf("%s", "The tribute funciton is working correctly for a one card deck.\n");
+
+        }
+        else {
+            printf("%s", "The tribute funciton is  not working correctly.\n");
+
+        }
+
+    }
+    
+
+
+return 0;
+
+
+
+
+}
